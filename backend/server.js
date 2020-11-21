4dea1cc002ebdc05a7c58a1a8e8aad5ada9b0f3c
seounjin <invanda7@gmail.com>
2020-11-21 16:28:10 +0900

    swagger add

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 5000;
 
 
+
 app.get('/', (req, res) => res.send('Hello World'));
+
+app.use(express.static('public'));
+
 
 app.listen(port, () => {
     console.log("hello world");
