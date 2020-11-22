5b0244af35c494a525033a337d300c83fa5780e5
seounjin <invanda7@gmail.com>
2020-11-22 18:50:45 +0900

    react add

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 const port = 5000;
 
 
+app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello  World'));
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 
 app.listen(port, () => {
